c7a4efea2f2a79519360b0c6dac1706e27b5350c
saiful <saiful121968@gmail.com>
2018-01-05 14:13:42 -0500

    First file added

---
+++ src/Green_1/Blue_1.java
@@ -0,0 +1,15 @@
+package Green_1;
+
+public class Blue_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello World!");
+		/* 8. */ System.out.print("Welcome to Java Programming");
+		/* 9. */  System.out.print("\nThis is my First Java Program");
+
+	}
+
+}
